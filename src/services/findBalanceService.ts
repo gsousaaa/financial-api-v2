a094153ddd8b76a157b5dd6054dd422b3b3b38db
gsousaaa <glaucoezequiel94@gmail.com>
2024-07-25 13:51:40 -0300

    fixing find balance method

--- src/services/findBalanceService.ts
+++ src/services/findBalanceService.ts
@@ -1,7 +1,6 @@
 import BadRequest from "@/errors/BadRequest"
 import { findMovements } from "@/repository/findMovements"
 import { findUser } from "@/repository/findUserByEmail"
-import { updateUserBalance } from "@/repository/updateUserBalance"
 
 export const findBalanceService = async (id: number) => {
     const movements = await findMovements(id)
@@ -9,19 +8,15 @@ export const findBalanceService = async (id: number) => {
 
     if(!user) throw new BadRequest('Usuário não encontrado!')
 
-    let balance = 0
     let expenses = 0
     let revenues = 0
 
     movements.forEach(movement => {
-        movement.movementType === 'revenue' ? balance += movement.value : balance -= movement.value
         movement.movementType === 'expense' ? expenses += movement.value : expenses += 0
         movement.movementType === 'revenue' ? revenues += movement.value : revenues += 0
     })
 
-    await updateUserBalance(id, balance)
-
-    return { balance, revenues, expenses }
+    return { balance: user.balance, revenues, expenses }
 }
 
 
