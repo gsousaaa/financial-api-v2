37aaec78fa530aca0cda35c84983a5563427b674
gsousaaa <glaucoezequiel94@gmail.com>
2024-07-26 19:04:30 -0300

    configuring the testing environment

--- src/database/config.ts
+++ src/database/config.ts
@@ -10,10 +10,12 @@ interface IDConfig {
     password: string;
     database: string;
     host: string;
+    port: string
 }
 
 interface IConfig {
     dev :IDConfig;
+    test: IDConfig;
     prod: IDConfig;
 }
 
@@ -23,22 +25,32 @@ const config: IConfig = {
         password: process.env.MYSQL_PASSWORD!,
         database: process.env.MYSQL_DB!,
         host: process.env.MYSQL_HOST!,
+        port: process.env.MYSQL_PORT!,
+    },
+
+    test: {
+        username: process.env.MYSQL_USER_TEST!,
+        password: process.env.MYSQL_PASSWORD_TEST!,
+        database: process.env.MYSQL_DB_TEST!,
+        host: process.env.MYSQL_HOST_TEST!,
+        port: process.env.MYSQL_PORT_TEST!,
     },
     prod: {
         username: process.env.DB_USER!,
         password: process.env.DB_USER_PASS!,
         database: process.env.DB_DATABASE!,
         host: process.env.DB_HOST!,
+        port: process.env.MYSQL_PORT!,
     }
 }
 
-const env = process.env.NODE_ENV || 'dev';
+const env = process.env.NODE_ENV;
 const dbConfig = config[env as keyof typeof config];
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: dbConfig.host,
-    port: parseInt(process.env.MYSQL_PORT as string),
+    port: parseInt(dbConfig.port),
     username: dbConfig.username,
     password: dbConfig.password,
     database: dbConfig.database,
@@ -48,3 +60,7 @@ export const AppDataSource = new DataSource({
     subscribers: [],
     migrations: [],
 });
+
+
+
+
