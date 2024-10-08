9de3284a52628dd804fa488cfac71a1f5d00b10e
gsousaaa <glaucoezequiel94@gmail.com>
2024-10-08 11:45:02 -0300

    fix: removing redis

--- src/database/redis.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@redis/client";
-
-export const client = createClient({
-     url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
-})
-
